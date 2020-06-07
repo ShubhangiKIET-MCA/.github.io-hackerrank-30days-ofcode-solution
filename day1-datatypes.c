1fe17e4d98a630c6e8107a8433220daca71ed3ab
Shubhangi <65587172+ShubhangiKIET-MCA@users.noreply.github.com>
2020-06-07 10:15:36 -0700

    Add files via upload

---
+++ day1-datatypes.c
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+int i2;
+ double d2;
+ char s2[100]; 
+
+ scanf("%d", &i2);
+ scanf("%lf", &d2);
+ scanf("%*[\n] %[^\n]", s2); 
+ 
+ printf("%d\n", i + i2);
+ printf("%.01lf\n", d + d2);
+ printf("%s%s", s, s2);
+return 0;
+}
+
+
+
